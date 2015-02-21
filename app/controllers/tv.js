60993bc919d8e3e4b25794ccdced9251ff662647
Arthur Flachs <arthur.flachs@gmail.com>
2015-02-21 18:26:58 +0100

    Change tv server address to 127.0.0.1:3000

--- app/controllers/tv.js
+++ app/controllers/tv.js
@@ -1,6 +1,6 @@
 raspberryApp.controller('tvCtrl', function($scope, $location, $state) {
   $scope.a = "b";
-  var socket = io("http://192.168.1.21:3000");
+  var socket = io("http://127.0.0.1:3000");
   socket.on('started', function(application) {
     $state.go('youtube');
   });
